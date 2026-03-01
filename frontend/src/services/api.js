dd13a1b7e1f1763fe741521b6bbc289a3bf2ad5a
premrajanand87@gmail.com <108508635+Premraj-87@users.noreply.github.com>
2026-03-01 12:53:29 +0530

    Update API baseURL to Render production link

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Backend base URL
+    baseURL: 'https://fitforge-fo5r.onrender.com/api', // Backend base URL
     headers: {
         'Content-Type': 'application/json',
     },
